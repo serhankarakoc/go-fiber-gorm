496087667daa8c99bd05a0da7af21d40e5971935
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-06-30 10:22:21 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module fiber-gorm
+
+go 1.15
+
+require (
+	github.com/gofiber/fiber/v2 v2.0.0
+	github.com/jinzhu/gorm v1.9.12
+)
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fiber-gorm/book"
+	"fiber-gorm/database"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	app.Use(cors.New())
+
+	initDatabase()
+	defer database.DBConn.Close()
+
+	setupRoutes(app)
+
+	log.Fatal(app.Listen(":3000"))
+}
